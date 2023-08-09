6299b10e33291dc7cd08736cf6eaf726187424b2
tia480 <tiaro121@gmail.com>
2023-08-09 22:19:02 +0100

    Update java.js

--- java.js
+++ java.js
@@ -49,7 +49,7 @@ function handleSubmit(event) {
 let form = document.querySelector(".search");
 form.addEventListener("submit", handleSubmit);
 
-function displayForecast(response){
+function displayForecast(){
   let forecastElement =document.querySelector("#weather-forecast")
   let forecastHTML= `<div class= "row">`;
   let days= ["Wed","Thurs","Fri ","sat","Sun","Mon"]
@@ -71,6 +71,7 @@ function displayForecast(response){
 }
 
 
+displayForecast();
 function getForecast(coordinates){
  let apiKey = "c119ffef35b7245a5e03b6e5724ae961";
  let apiURL= `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -150,6 +151,7 @@ fahrenheitLink.classList.remove("active");
 let temperatureElement = document.querySelector(".Current-temp");
 temperatureElement.innerHTML = Math.round(CelsiusTemperature)
 };
+console.log(displayCelsiusTemperature);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link")
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
