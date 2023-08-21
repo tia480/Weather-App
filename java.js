44085099a67fae843e9150f8db512aa4f98d0688
tia480 <tiaro121@gmail.com>
2023-08-21 21:04:55 +0100

    Display forecast using the API

--- java.js
+++ java.js
@@ -48,20 +48,38 @@ function handleSubmit(event) {
 
 let form = document.querySelector(".search");
 form.addEventListener("submit", handleSubmit);
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function formatDay (timestamp){
+  let date = new Date (timestamp*1000)
+  let day = date.getDay();
+  let days =["Sun","Mon","Tue", "Wed","Thu","Fri","Sat"]
+  
+  return days[day];
+}
+
 
 function displayForecast(response){
-  let forcast = response.data.daily;
+  let forecast = response.data.daily;
   let forecastElement =document.querySelector("#weather-forecast")
   let forecastHTML= `<div class= "row">`;
-  let days= ["Wed","Thurs","Fri ","sat","Sun","Mon"]
-  days.forEach(function(forecastDay){
+  forecast.forEach(function(forecastDay, index){
+   if (index < 6)
   forecastHTML = forecastHTML +
  `<div class="col-2">
-    <div class="forecast-day-1">${forecastDay.dt}</div>
-      🌤️${forecastDay.weather[0].icon}
+    <div class="forecast-day-1">${formatDay(forecastDay.dt)}</div>
+    <div>
+  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png" alt="WeatherIcon">
+</div>
        <div class="forecast-temp">
-       <span class="forecast-temp-max">${forecastDay.temp.max}° | </span>
-      <span class="forecast-temp-min"> ${forecastDay.temp.min}° </span>
+       <span class="forecast-temp-max">${Math.round(forecastDay.temp.max)}°| </span>
+      <span class="forecast-temp-min"> ${Math.round(forecastDay.temp.min)}° </span>
    </div>
    </div>`;   
   })
@@ -72,14 +90,12 @@ function displayForecast(response){
 }
 
 
-displayForecast();
 function getForecast(coordinates){
  let apiKey = "63214c4281922e3bb72fdf12dada7734";
  let apiURL= `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
  axios.get(apiURL).then(displayForecast)
  console.log(coordinates);
 };
-
 
 
 function currentTemp(response) {
@@ -163,3 +179,4 @@ let celsiusLink = document.querySelector("#celsius-link")
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
+search("italy");
